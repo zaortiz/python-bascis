cfe2ca16e757a4903751d5db5eb3814b64df8578
Ortiz <zaortiz@cps.k12.il.us>
2024-05-07 09:14:26 -0500

    loops2

---
+++ pythonLearing/whilepractice.py
@@ -0,0 +1,23 @@
+#while loops allow you to repeat code until a condition is met
+#while loops are defined with the while keyword
+#while loops are used when you don't know how many 
+#times you need to repeat the code
+# studentList = ["citlali", "praise", "Aaron", "Fiona", "Xavier"]
+# studentName = 0
+# while studentName < len(studentList):
+#     print(studentList[studentName])
+#     studentName+= 1
+
+# fruitList = ["apple", "bannana", "cherry", "date", "elderberry"]
+
+# #for loop to print the fruitList
+# #for loop is when you know how many times
+# #you need to repeat the code
+# for fruit in fruitList:
+#     print(fruit + " is a fruit.")
+
+# StudentList = ["citlali", "Praise", "Aaron", "Fiona", "Xavier"]
+
+# for student in StudentList:
+#     ask = input("what is your last name?")
+#     print(student + "" + ask)
---
+++ whileLoops.py
@@ -0,0 +1,31 @@
+#nested for loops used when you knoe exact number of things
+# nums = range(1, 10000)
+# for num in nums :
+#     for letter in 'abc':
+#         for letter2 in 'abc':
+#             for letter3 in 'abc':
+#                 for letter4 in 'abc':
+#                     print(num, letter, letter2, letter3, letter4)
+
+#while loops
+#when you do not know the number of iterations
+
+# for i in range(1, 11):
+#     print(i)
+
+# x = 0
+
+# while True:
+#     if x ==5:
+#         break
+#     print(x)
+#     x += 1
+
+# while True:
+#     print("zayla")
+#     name = input()
+#     if name == 'zayla':
+#         print("zayla" +name)
+#         break
+#     else:
+#         print("thank you!")
